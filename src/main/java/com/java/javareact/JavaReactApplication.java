e76664038c607fbf4b1e0f81842f52d521175785
wildwing <ggipk1010@gmail.com>
2021-02-27 21:18:22 +0900

    Initial commit

---
+++ src/main/java/com/java/javareact/JavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.java.javareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/javareact/JavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.javareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
