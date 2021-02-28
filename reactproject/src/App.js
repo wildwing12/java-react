c5d8df2740739ef4097476048f06cf9cbef0d4c5
wildwing <ggipk1010@gmail.com>
2021-02-28 21:05:07 +0900

    로그인페이지 컨테이너 디렉토리로 옮김

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import LoginPage from './Component/LoginPage';
+import LoginPage from './container/LoginPage';
 
 function App() {
 	return (
